be75ed0c7801744cd803ca20531cbb66161b720d
Pablo <ppoloalc@alumnos.unex.es>
2022-10-26 15:22:28 +0200

    Comparadores

---
+++ ComparadorCiclistasEnergia.java
@@ -0,0 +1,17 @@
+
+/**
+ * Java class to demonstrate the use of Java Comparator.  
+ * Creating a class which implements Comparator Interface (using 
+ * name field)
+ */ 
+import java.util.*;  
+class ComparadorCiclistasEnergia implements Comparator<Ciclista>{  
+    public int compare(Ciclista c1,Ciclista c2){  
+        if(c1.getEnergia()==c2.getEnergia())  
+            return 0;  
+        else if(c1.getEnergia()>c2.getEnergia())  
+            return 1;  
+        else  
+            return -1;  
+    }  
+}  
