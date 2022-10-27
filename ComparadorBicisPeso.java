6985ecd80d4006bb4cd7998457d7f7e2c2253d15
Jesus Cerrudo <jcerrudo@alumnos.unex.es>
2022-10-27 10:33:34 +0200

    comparadores 2

--- ComparadorBicisPeso.java
+++ ComparadorBicisPeso.java
@@ -7,7 +7,7 @@ import java.util.*;
 class ComparadorBicisPeso implements Comparator<Bicicleta>{  
     public int compare(Bicicleta b1,Bicicleta b2){  
         if(b1.getPeso()==b2.getPeso())  
-            return 0;  
+            return b1.getName().compareTo(b2.getName());  
         else if(b1.getPeso()>b2.getPeso())  
             return 1;  
         else  
--- ComparadorCiclistasHabilidad.java
+++ ComparadorCiclistasHabilidad.java
@@ -5,13 +5,17 @@
  * name field)
  */ 
 import java.util.*;  
-class ComparadorCiclistasEnergia implements Comparator<Ciclista>{  
+class ComparadorCiclistasHabilidad implements Comparator<Ciclista>{  
+  
     public int compare(Ciclista c1,Ciclista c2){  
-        if(c1.getEnergia()==c2.getEnergia())  
+        if(c1.getHabilidad()==c2.getHabilidad())  
             return c1.getName().compareTo(c2.getName());  
-        else if(c1.getEnergia()>c2.getEnergia())  
+        else if(c1.getHabilidad()>c2.getHabilidad())  
             return 1;  
         else  
             return -1;  
     }  
 }  
+
+    
+
