dd1340f6625854a21b24f29bd9b0a14bf396fd8b
Marco Vega <mvegavel@alumnos.unex.es>
2022-12-11 16:39:17 +0100

    Correcion errores ordenacion resultados

--- compResultadosTiempo.java
+++ compResultadosTiempo.java
@@ -5,7 +5,8 @@ import java.util.*;
 class compResultadosTiempo implements Comparator<Resultados>{  
     public int compare(Resultados r1,Resultados r2){  
         if(r1.getTiempo()==r2.getTiempo())  
-            return 0;  
+         return r1.getCiclista().getName().compareTo(r2.getCiclista().getName());     
+        //return 0;  
         else if(r1.getTiempo()>r2.getTiempo())  
             return 1;  
         else   
